9b3a6edd89e770c13914a8f434f25db308561420
DESKTOP-CK4D7B4\PC <dnwlsrla40@naver.com>
2020-11-05 16:54:09 +0900

    Server second Commit

---
+++ back/pochat/src/main/java/com/woojin/pochat/PochatApplication.java
@@ -0,0 +1,13 @@
+package com.woojin.pochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PochatApplication.class, args);
+	}
+
+}
---
+++ back/pochat/src/test/java/com/woojin/pochat/PochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojin.pochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
